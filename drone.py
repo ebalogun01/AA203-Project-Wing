cd07c6ed3dbbc68a1efaa594fc53302b1ccc11e0
Courtney Moran <courtney@courtnesmacbook.lan>
2020-06-02 22:57:51 -0500

    a;dslkj

--- drone.py
+++ drone.py
@@ -1,12 +1,11 @@
 class drone(object):
 
-    def __init__(self,center,destination,weight,charge,speed=13,radius=1):
-        self.center = center            #position of drone center w.r.t. the map
+    def __init__(self,id,position,destination,weight,charge):
+        self.id = id                    #id number to distinguish b/w drones
+        self.position = position        #position of drone center w.r.t. the map
         self.destination = destination  #package destination or depot
         self.weight = weight            #weight of drone + package
         self.charge = charge            #current battery charge
-        self.speed = speed              #constant speed of drone during delivery
-        self.radius = radius            #distance from center to widest point
         self.path = None
 
     def add_package(self,package_weight):
