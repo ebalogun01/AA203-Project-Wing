dc6bd0d0a42eba59201a8d314deb213c4f9938e7
Courtney Moran <courtney@courtnesmacbook.lan>
2020-06-07 18:15:11 -0500

    milp function

--- milp.py
+++ milp.py
@@ -1,37 +1,52 @@
 import cvxpy as cp
 
 """
-X -> x_ij=1 if drone i is assigned to delivery j
-cost -> total length of delivery routes
-d -> d_ij is the distance for drone i to deliver to request j
+run_milp:   minimize total distance travelled by all drones
+            output determines delivery-drone assignment
+input:
+    available_drones -> list of drone objects that are free @ a depot or
+                        on their way back from a previous delivery
+    delivery_list    -> list of coordinates of delivery requests
+    depot_locs       -> list of coordinates of depot locations
+    paths            -> table with precomputed path lengths
+variables:
+    X -> x_ij=1 if drone i is assigned to delivery j
+    cost -> total length of delivery routes
+    d -> d_ij is the distance for drone i to deliver to request j
+
+output:
+    [(delivery_idx,id),...] ->  returns a list of tuples of the index of the
+                                delivery in the delivery_list & the id of the
+                                drone assigned to it
 
 """
-def run_milp(drones_list, delivery_test, depot_locations, paths):
-m = len(available_drones)
-n = len(delivery_list)
-d = np.zeros((m,n))
-for i,drone in available_drones:
-    for j,delivery in delivery_list:
-        if drone.status==0:
-            d[i,j] = path_dist(drone.position,delivery)
-        else if drone.status==4:
-            for depot in depot_locs:
-                d = path_dist(drone.position,depot)+path_dist(depot,delivery)
-                if d<d[i,j]:
-                    d[i,j]==d
+def run_milp(available_drones, delivery_list, depot_locs, paths):
+    ## fill in matrix of distances, d
+    m = len(available_drones)
+    n = len(delivery_list)
+    d = np.zeros((m,n))
+    for i,drone in available_drones:
+        for j,delivery in delivery_list:
+            if drone.status==0:
+                d[i,j] = path_dist(drone.position,delivery)
+            else if drone.status==4:
+                for depot in depot_locs:
+                    d = path_dist(drone.position,depot)+path_dist(depot,delivery)
+                    if d<d[i,j]:
+                        d[i,j]==d
 
+    ## CVX solve
+    X = cp.Variable(m,n,boolean=True)
+    cost = cp.sum(cp.multiply(X,d))
+    constraints = [cp.sum(X,axis=0)<=1,cp.sum(X,axis=1)<=1]
+    p = cp.Problem(cp.Minimize(cost),constraints)
+    p.solve()
 
-
-X = cp.Variable(m,n,boolean=True)
-cost = cp.sum(cp.multiply(X,d))
-constraints = 
-p = cp.Problem(cp.Minimize(cost))
-p.solve()
-
-
-
-"""
-Need to define:
-    available_drones    -> list of drones not currently delivering a package
-    path_dist           -> function that returns a* path length b/w 2 points
-"""
+    ## return list of assignment pairs
+    X_val = X.value
+    assignments = []
+    for i in range(m):
+        j = np.argwhere(X_val[i,:]>0)
+        if j.shape[0]>0:
+            assignment.append((j[0,0],available_drones[i].id))
+    return assignments
